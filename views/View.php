4bbcd2927a251c701f3eec498a2ac512e3639856
nicovrg <nicolasvergne88@gmail.com>
2019-10-28 02:56:40 +0100

    add htaccess and view class

---
+++ views/View.php
@@ -0,0 +1,32 @@
+<?php
+class View
+{
+	private $_t;
+	private $_file;
+
+	public function __construct($action)
+	{
+		$this->_file = 'views/view'.$action.'php';
+	}
+
+	public function generate($data)
+	{
+		$content = $this->generateFile($this->_file, $data);
+		$view = $this->generateFile('views/template.php', array('t' => $this->$_t, 'content' => $content));
+		echo $view;
+	}
+
+	private function generateFile($file, $date)
+	{
+		if (file_exists($file))
+		{
+			extract($data);
+			ob_start();
+			require $file;
+			return ob_get_clean();
+		}
+		else
+			throw new Exception('File '.$file.' not found');
+	}
+}
+?>
