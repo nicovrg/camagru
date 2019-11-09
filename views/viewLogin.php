29c67a534ad15db44682ecdad8a34f76048b5559
nicovrg <nicolasvergne88@gmail.com>
2019-11-09 10:49:48 +0100

    a little big of style on login register modify

--- views/viewLogin.php
+++ views/viewLogin.php
@@ -1,11 +1,9 @@
 <?php $this->_t = "Login" ?>
 <div class="login">
-	<p class="login_title">login</p>
-	<form action="/login" method="POST" id="login">
-		<p class="login_field_name">username: </p>
-		<input class="login_form_cell" type="text" name="username">
-		<p class="login_field_name">password: </p>
-		<input class="login_form_cell" type="text" name="password">
-		<input class="login_form_cell" type="submit" name="confirm">
+	<h3 class="login_title">login</h3>
+	<form action="/login" method="POST" class="login">
+		username: <input class="login_form_cell" type="text" name="username">
+		password: <input class="login_form_cell" type="text" name="password">
+		<input type="submit" name="confirm">
 	</form>
 </div>
--- views/viewRegister.php
+++ views/viewRegister.php
@@ -1,7 +1,7 @@
 <?php $this->_t = "Register" ?>
 <div>
-	<p style="display:flex; justify-content: center;">register</p>
-	<form action="/register" method="POST" id="register">
+	<h3 class="register_title">register</h3>
+	<form action="/register" method="POST" class="register">
 		username: <input class="register_form_cell" type="text" name="username">
 		email: <input class="register_form_cell" type="text" name="email">
 		password: <input class="register_form_cell" type="text" name="password">
