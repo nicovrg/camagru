f22cda7935748a9d9b28df900f2ecaf86e41bc87
nicovrg <nicolasvergne88@gmail.com>
2019-11-22 03:41:14 +0100

    edit homepage css

--- views/viewHomepage.php
+++ views/viewHomepage.php
@@ -1,15 +1,13 @@
 <?php $this->_t = "Home" ?>
 <div class="gallery">
-	<?php
-		foreach ($pictures as $picture)
-		{
-			echo "<div class='gallery_element'>";
-			echo "<img src='/img/" . $picture->name() . "' class='gallery_element_img'>";
-			echo "<p>Like: " . "" . "Comments: " . "" . "Name:" . $picture->name() . "</p>";
-			echo "</div>";
-			$i++;
-		}
-	?>
+	<?php foreach ($pictures as $picture): ?>
+	<div class='gallery_elements'>
+		<div class='gallery_element'>
+			<img src="/img/<?= $picture->name() ?>" class='gallery_element_img'>
+			<p>Like: Comments: <?= $picture->name() ?> </p>
+		</div>
+	</div>
+	<?php endforeach; ?>
 	<!-- hide or show image in full screen -->
 	<div class="modal"> 
 		<img class="modal-content">
